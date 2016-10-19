a90d31337e54de44808c5d428de3b1613c23016f
mohit <mohit_saindane@persistent.com>
2016-10-19 13:57:54 +0530

    checkin for technothon 14

--- src/main/java/com/psl/Sherlock/controller/HomeController.java
+++ src/main/java/com/psl/Sherlock/controller/HomeController.java
@@ -22,11 +22,17 @@ import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.html.FormSubmitEvent.MethodType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.context.annotation.PropertySource;
+
 import org.springframework.core.env.Environment;
+
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
