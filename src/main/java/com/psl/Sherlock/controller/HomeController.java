c1d662e493f6c7c6ba6e925b86fe82e54bee6f4d
mohit <mohit_saindane@persistent.com>
2016-10-19 13:04:45 +0530

    checkin for technothon 05

--- src/main/java/com/psl/Sherlock/controller/HomeController.java
+++ src/main/java/com/psl/Sherlock/controller/HomeController.java
@@ -8,41 +8,27 @@ import java.lang.reflect.Method;
 import java.sql.SQLException;
 import java.util.Base64;
 import java.util.List; 
-
 import java.util.Base64;
-
 import java.util.List;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.swing.text.html.FormSubmitEvent.MethodType;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-
-
 import org.springframework.core.env.Environment;
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.psl.Sherlock.entity.Citizen;
 import com.psl.Sherlock.entity.CreditHistory;
-
 import com.psl.Sherlock.entity.Criminal;
 import com.psl.Sherlock.service.CitizenService;
 import com.psl.Sherlock.service.CreditHistoryService;
