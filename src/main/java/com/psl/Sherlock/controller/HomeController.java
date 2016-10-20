1c04b0aae590a98a2552301314779f1eaa86b5ce
mohit <mohit_saindane@persistent.com>
2016-10-20 14:41:38 +0530

    checkin for technothon 53

--- src/main/java/com/psl/Sherlock/controller/HomeController.java
+++ src/main/java/com/psl/Sherlock/controller/HomeController.java
@@ -33,14 +33,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
-
-
-
-
-
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.support.XmlWebApplicationContext;
+
+
+
+
 
 
 import org.springframework.web.servlet.ModelAndView;
